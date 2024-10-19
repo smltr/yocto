b3c171b83f478db4b83df8e388ffc6e042938498
Sam Trouy <trouys16@gmail.com>
2024-10-18 22:03:39 -0400

    repl

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -32,13 +33,49 @@ func EvalString(input string) (string, error) {
 }
 
 func main() {
-	// Example usage: go run main.go example.yoc
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: yocto <filename.yoc>")
+	if len(os.Args) == 1 {
+		repl()
+	} else if len(os.Args) == 2 {
+		runFile(os.Args[1])
+	} else {
+		fmt.Println("Usage: yocto [filename.yoc]")
 		os.Exit(1)
 	}
+}
 
-	filename := os.Args[1]
+func repl() {
+	reader := bufio.NewReader(os.Stdin)
+	env := NewEnvironment(nil)
+	for {
+		fmt.Print("> ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			break
+		}
+		tokens := tokenize(input)
+		var result Expression
+		var err error
+		for len(tokens) > 0 {
+			var expr Expression
+			expr, tokens, err = parseExpr(tokens)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				break
+			}
+			result, err = expr.Evaluate(env)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				break
+			}
+		}
+		if err == nil {
+			fmt.Println(result)
+		}
+	}
+}
+
+func runFile(filename string) {
 	if !strings.HasSuffix(filename, ".yoc") {
 		fmt.Println("Error: File must have .yoc extension")
 		os.Exit(1)
@@ -50,11 +87,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Add built-in functions to the environment
 	_, err = EvalString(string(contents))
 	if err != nil {
 		fmt.Printf("Error evaluating file: %v\n", err)
 		os.Exit(1)
 	}
-
 }
